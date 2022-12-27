a68770de567571421789f29ab46aa9f9ce49915c
ngocban <daongocban1102@gmail.com>
2022-12-27 22:52:44 +0700

    basic server to create laptop

--- GO/go.mod
+++ GO/go.mod
@@ -3,7 +3,15 @@ module pcbook
 go 1.19
 
 require (
-	github.com/golang/protobuf v1.5.0
+	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.3.0
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
+	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
+)
+
+require (
+	golang.org/x/net v0.0.0-20220722155237-a158d28d115b // indirect
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f // indirect
+	golang.org/x/text v0.4.0 // indirect
 )
--- GO/service/laptop_server.go
+++ GO/service/laptop_server.go
@@ -1,8 +1,47 @@
 package service
 
+import (
+	"context"
+	"log"
+	pb "pcbook/generateProto"
+
+	"github.com/google/uuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
 type LaptopServer struct {
 }
 
 func NewLaptopServer() *LaptopServer {
 	return &LaptopServer{}
+}
+
+func (s *LaptopServer) CreateLaptop(
+	ctx context.Context,
+	req *pb.CreateLaptopRequest,
+	opts ...grpc.CallOption) (
+	*pb.CreateLaptopResponse,
+	error) {
+	laptop := req.GetLaptop()
+	log.Print("Received a request to create a laptop with ID: ", laptop.Id)
+
+	if len(laptop.Id) > 0 {
+		_, err := uuid.Parse(laptop.Id)
+		if err != nil {
+			return nil, status.Errorf(codes.InvalidArgument, "laptop id is not a valid UUID : %v", err)
+		}
+	} else {
+		id, err := uuid.NewRandom()
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "cannot generate a new laptop ID: %v", err)
+		}
+		laptop.Id = id.String()
+
+	}
+	return &pb.CreateLaptopResponse{
+		Id: laptop.Id,
+	}, nil
+
 }
