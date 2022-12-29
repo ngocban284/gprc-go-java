1c628fa2235873449d4c9c99d8bb21607a988a8f
ngocban <daongocban1102@gmail.com>
2022-12-29 21:17:36 +0700

    client dial server basic

--- GO/cmd/client/main.go
+++ GO/cmd/client/main.go
@@ -1,10 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"flag"
+	"log"
+	pb "pcbook/generateProto"
+	"pcbook/sample"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // hello world
 func main() {
-	fmt.Println("Hello World From Client")
+	serverAddress := flag.String("serverAddress", "", "server address")
+	flag.Parse()
+	log.Print("dial server: ", *serverAddress)
+
+	conn, err := grpc.Dial(*serverAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("cannot dial server: ", err)
+	}
+	defer conn.Close()
+
+	laptopClient := pb.NewLaptopServiceClient(conn)
+	laptop := sample.NewLaptop()
+
+	req := &pb.CreateLaptopRequest{
+		Laptop: laptop,
+	}
+	res, err := laptopClient.CreateLaptop(context.Background(), req)
+	if err != nil {
+		st, ok := status.FromError(err)
+		if ok {
+			if st.Code() == codes.AlreadyExists {
+				log.Print("laptop already exists")
+
+			} else {
+				log.Fatal("cannot create laptop: ", err)
+			}
+			return
+		}
+	}
+	log.Print("create laptop with id: ", res.Id)
+
 }
