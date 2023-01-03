f1954e20357003f60615ea544c65ff4c012076ab
ngocban <daongocban1102@gmail.com>
2023-01-03 10:26:56 +0700

    search laptop client service unit test

--- GO/service/laptop_client_test.go
+++ GO/service/laptop_client_test.go
@@ -39,7 +39,73 @@ func TestLaptopClientCreateLaptop(t *testing.T) {
 
 	requireSameLaptop(t, laptop, other)
 }
+func TestLaptopClientSearchLaptop(t *testing.T) {
+	t.Parallel()
 
+	filter := &pb.Filter{
+		MaxPriceUsd: 2000,
+		MinCpuCores: 4,
+		MinCpuGhz:   2.2,
+		MinRam:      &pb.Memory{Value: 8, Unit: pb.Memory_GIGABYTE},
+	}
+
+	laptopStore := service.NewInMemoryLaptopStore()
+	expectedID := make(map[string]bool)
+
+	for i := 0; i < 6; i++ {
+		laptop := sample.NewLaptop()
+
+		switch i {
+		case 0:
+			laptop.PriceUsd = 2500
+		case 1:
+			laptop.Cpu.NumberCores = 2
+		case 2:
+			laptop.Cpu.MinGhz = 2.0
+		case 3:
+			laptop.Ram = &pb.Memory{Value: 4096, Unit: pb.Memory_MEGABYTE}
+		case 4:
+			laptop.PriceUsd = 1999
+			laptop.Cpu.NumberCores = 4
+			laptop.Cpu.MinGhz = 2.5
+			laptop.Cpu.MaxGhz = 4.5
+			laptop.Ram = &pb.Memory{Value: 16, Unit: pb.Memory_GIGABYTE}
+			expectedID[laptop.Id] = true
+		case 5:
+			laptop.PriceUsd = 2000
+			laptop.Cpu.NumberCores = 6
+			laptop.Cpu.MinGhz = 2.8
+			laptop.Cpu.MaxGhz = 5.0
+			laptop.Ram = &pb.Memory{Value: 64, Unit: pb.Memory_GIGABYTE}
+			expectedID[laptop.Id] = true
+		default:
+			panic("should not reach here")
+		}
+
+		err := laptopStore.Save(laptop)
+		require.NoError(t, err)
+	}
+
+	serverAddress := startTestLaptopServer(t, laptopStore)
+	laptopClient := newTestLaptopClient(t, serverAddress)
+
+	req := &pb.SearchLaptopRequest{
+		Filter: filter,
+	}
+	stream, err := laptopClient.SearchLaptop(context.Background(), req)
+	require.NoError(t, err)
+	found := 0
+	for {
+		res, err := stream.Recv()
+		if err != nil {
+			break
+		}
+		require.NoError(t, err)
+		require.Contains(t, expectedID, res.Laptop.Id)
+		found++
+	}
+	require.Equal(t, len(expectedID), found)
+}
 func startTestLaptopServer(t *testing.T, laptopStore service.LaptopStore) string {
 	laptopServer := service.NewLaptopServer(laptopStore)
 
