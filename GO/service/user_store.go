26381798caf227176d95f3153083c5249673ac86
ngocban <daongocban1102@gmail.com>
2023-01-05 16:30:51 +0700

    user store

---
+++ GO/service/user_store.go
@@ -0,0 +1,46 @@
+package service
+
+import (
+	"fmt"
+	"sync"
+)
+
+type UserStore interface {
+	Save(user *User) error
+	Find(userName string) (*User, error)
+}
+
+type InMemoryUserStore struct {
+	mutex sync.RWMutex
+	users map[string]*User
+}
+
+func NewInMemoryUserStore() *InMemoryUserStore {
+	return &InMemoryUserStore{
+		users: make(map[string]*User),
+	}
+}
+
+func (store *InMemoryUserStore) Save(user *User) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if store.users[user.UserName] != nil {
+		return fmt.Errorf("user %s already exists", user.UserName)
+	}
+
+	store.users[user.UserName] = user
+	return nil
+}
+
+func (store *InMemoryUserStore) Find(userName string) (*User, error) {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	user := store.users[userName]
+	if user == nil {
+		return nil, fmt.Errorf("user %s not found", userName)
+	}
+
+	return user.Clone(), nil
+}
